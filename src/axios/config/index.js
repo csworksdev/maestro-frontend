dcf8a1540af1074fbd4a859541bae97499886b22
csworksdev <csworksdev@gmail.com>
2024-10-01 14:38:35 +0700

    idem

--- src/axios/config/index.js
+++ src/axios/config/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosConfig = axios.create({
   // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "http://34.101.215.88/",
+  baseURL: "https://34.101.215.88/",
   // baseURL: "http://127.0.0.1:8000/",
 });
