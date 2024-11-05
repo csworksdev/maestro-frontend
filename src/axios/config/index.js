7354cd8a2c113be7b26d9edf9ea610100403c068
csworksdev <csworksdev@gmail.com>
2024-11-05 10:15:04 +0700

    change base url

--- src/axios/config/index.js
+++ src/axios/config/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosConfig = axios.create({
   // baseURL: import.meta.env.VITE_API_URL,
-  // baseURL: "https://api.maestroswim.com/", //change to https
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://api.maestroswim.com/", //change to https
+  // baseURL: "http://127.0.0.1:8000/",
 });
