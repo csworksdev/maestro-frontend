2aa139d7b1934537612dbac3c6c7f56eddc646e9
csworksdev <csworksdev@gmail.com>
2025-07-11 11:08:37 +0700

    migrate pnpm

--- vite.config.js
+++ vite.config.js
@@ -5,62 +5,59 @@ import replace from "@rollup/plugin-replace";
 import viteCompression from "vite-plugin-compression";
 import { visualizer } from "rollup-plugin-visualizer";
 
-export default defineConfig(({ mode }) => {
-  return {
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-      },
+export default defineConfig(({ mode }) => ({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    plugins: [
-      replace({
-        preventAssignment: true,
-        values: {
-          __DEV__: JSON.stringify(true),
-          "process.env.NODE_ENV": JSON.stringify("development"),
-        },
-      }),
-      react(),
-      ...(mode === "production"
-        ? [
-            viteCompression(),
-            visualizer({
-              filename: "./dist/stats.html",
-              open: true,
-            }),
-          ]
-        : []),
-    ],
-    server: {
-      host: "localhost",
-      port: 3001,
-      open: true,
-      strictPort: true,
-      watch: {
-        usePolling: true,
-        interval: 500,
-        ignored: ["**/node_modules/**"],
+  },
+  plugins: [
+    replace({
+      preventAssignment: true,
+      values: {
+        __DEV__: JSON.stringify(mode !== "production"),
+        "process.env.NODE_ENV": JSON.stringify(mode),
       },
-      hmr: true,
+    }),
+    react(),
+    ...(mode === "production"
+      ? [
+          viteCompression(),
+          visualizer({
+            filename: "./dist/stats.html",
+            open: false, // false agar gak bikin CI fail
+          }),
+        ]
+      : []),
+  ],
+  server: {
+    host: "localhost",
+    port: 3001,
+    open: true,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 500,
+      ignored: ["**/node_modules/**"],
     },
-    build: {
-      sourcemap: false, // true if you want easier debugging
+  },
+  build: {
+    sourcemap: false,
+    target: "esnext",
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
+  esbuild: {
+    sourcemap: true,
+    target: "esnext",
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+    esbuildOptions: {
       target: "esnext",
-      outDir: "dist",
-      emptyOutDir: true,
-      rollupOptions: {
-        input: path.resolve(__dirname, "index.html"),
-      },
     },
-    esbuild: {
-      sourcemap: true, // true if you want easier debugging
-      target: "esnext",
-    },
-    optimizeDeps: {
-      include: ["react", "react-dom"], // tambahkan dep utama kamu
-      esbuildOptions: {
-        target: "esnext",
-      },
-    },
-  };
-});
+  },
+}));
