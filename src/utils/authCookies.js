7ed116302f0b799dff8bbd18a605604c8e87af34
csworksdev <csworksdev@gmail.com>
2025-10-30 09:27:23 +0700

    fix : security issue

--- src/utils/authCookies.js
+++ src/utils/authCookies.js
@@ -74,7 +74,7 @@ export const setCookie = (
 export const getCookie = (name) => {
   if (!isBrowser()) return null;
   const match = document.cookie.match(
-    new RegExp(`(?:^|; )${name.replace(/([.$?*|{}()[\]\/+^])/g, "\$1")}=([^;]*)`)
+    new RegExp(`(?:^|; )${name.replace(/([.$?*|{}()[\]\/+^])/g, "\\$1")}=([^;]*)`)
   );
   return match ? decodeURIComponent(match[1]) : null;
 };
