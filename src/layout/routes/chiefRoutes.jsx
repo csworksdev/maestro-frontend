3812000c49c07483fb61c5400322702c1234267d
csworksdev <csworksdev@gmail.com>
2025-11-03 15:34:25 +0700

    feat : dashboard ceo

---
+++ src/layout/routes/chiefRoutes.jsx
@@ -0,0 +1,52 @@
+import React, { lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Layout from "../Layout";
+import AuthLayout from "../AuthLayout";
+import PublicRoute from "../PublicRoute";
+import AuthenticatedRoute from "../AuthenticatedRoute";
+
+const LoginAdmin = lazy(() => import("@/pages/auth/login"));
+const RegisterAdmin = lazy(() => import("@/pages/auth/register"));
+const ForgotPassAdmin = lazy(() => import("@/pages/auth/forgot-password"));
+
+const CEODashboard = lazy(() => import("@/pages/newDashboard/ceo"));
+const NotificationPage = lazy(() => import("@/pages/utility/notifications"));
+const ErrorPage = lazy(() => import("@/pages/404"));
+
+const ChiefRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/auth"
+        element={
+          <PublicRoute>
+            <AuthLayout />
+          </PublicRoute>
+        }
+      >
+        <Route path="login" element={<LoginAdmin />} />
+        <Route path="register" element={<RegisterAdmin />} />
+        <Route path="forgot-password" element={<ForgotPassAdmin />} />
+      </Route>
+
+      <Route
+        path="/"
+        element={
+          <AuthenticatedRoute>
+            <Layout />
+          </AuthenticatedRoute>
+        }
+      >
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard">
+          <Route index element={<CEODashboard />} />
+        </Route>
+        <Route index path="notifications" element={<NotificationPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default ChiefRoutes;
