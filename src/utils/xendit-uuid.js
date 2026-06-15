555802bbaafe75ed5ad40170cd55d907497cbccf
csworksdev <csworksdev@gmail.com>
2026-06-15 14:44:54 +0700

    ganti xendit uuid macox -> mssc

--- src/utils/xendit-uuid.js
+++ src/utils/xendit-uuid.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
 export const generateExternalId = () => {
-  const externalId = `macox-${uuidv4()}`;
+  const externalId = `MSSC-${uuidv4()}`;
   return externalId;
 };
